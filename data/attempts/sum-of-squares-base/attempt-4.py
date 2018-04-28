d02b0a3403404ced20fd6d1a7f5ca124724968c2
Ruan Reis <ruanvictorreis@gmail.com>
2018-04-28 12:10:55 -0300

    setting all data

--- data/attempts/sum-of-squares-base/attempt-4.py
+++ data/attempts/sum-of-squares-base/attempt-4.py
@@ -1,6 +1,4 @@
 def sum_of_squares_base(base, n):
   total = base
   for i in range(n):
-    total = base + (i^2)
-    
-sum_of_squares_base(5,3)    		
+    total = base + (i^2)		
--- data/attempts/sum-of-squares-base/attempt-5.py
+++ data/attempts/sum-of-squares-base/attempt-5.py
@@ -2,6 +2,4 @@ def sum_of_squares_base(base, n):
   total = base
   for i in range(n):
     total = base + (i^2)
-  return total
-    
-sum_of_squares_base(5,3)    		
+  return total	
